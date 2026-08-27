102824566cae497327d1c64e72168652d88f46f3
hmarszalek <hanna.marszalek@scylladb.com>
2026-08-27 17:55:53 +0200

    Convert token to typescript
    A mechanical conversion: signatures and fields get their types, the
    JSDoc type tags that duplicate them are dropped, and the emitted
    token.js and token.d.ts join the other build output in lib/.gitignore.
    No behaviour changes.

--- lib/metadata/index.ts
+++ lib/metadata/index.ts
@@ -5,10 +5,11 @@
  * @module metadata
  */
 
-import { EmptyCallback, token, ValueCallback } from "../..";
+import { EmptyCallback, ValueCallback } from "../..";
 // TODO: remove once `lib/promise-utils.js` is converted to typescript.
 // @ts-ignore
 import promiseUtils = require("../promise-utils");
+import { Token, TokenRange } from "../token";
 import { Host } from "../host";
 import types = require("../types");
 import { ColumnInfo } from "../types/cql-utils";
@@ -80,23 +81,22 @@ class Metadata {
      * It uses the pre-loaded keyspace metadata to retrieve the replicas for a token for a given keyspace.
      * When the keyspace metadata has not been loaded, it returns null.
      * @param {string} keyspaceName Name of the keyspace.
-     * @param {Buffer | token.Token | token.TokenRange} token Can be Buffer (serialized partition key),
+     * @param {Buffer | Token | TokenRange} token Can be Buffer (serialized partition key),
      * Token or TokenRange.
      * @returns {Host[]} The replicas.
      */
     getReplicas(
         keyspaceName: string,
-        token: Buffer | token.Token | token.TokenRange,
+        token: Buffer | Token | TokenRange,
     ): Host[] {
         throw new Error("TODO: Not implemented");
     }
 
     /**
      * Gets the token ranges that define data distribution in the ring.
-     * @returns {Set<token.TokenRange>} The ranges of the ring or empty set if schema metadata is
-     * not enabled.
+     * @returns {Set<TokenRange>} The ranges of the ring or empty set if schema metadata is not enabled.
      */
-    getTokenRanges(): Set<token.TokenRange> {
+    getTokenRanges(): Set<TokenRange> {
         throw new Error("TODO: Not implemented");
     }
 
@@ -104,13 +104,13 @@ class Metadata {
      * Gets the token ranges that are replicated on the given host, for the given keyspace.
      * @param {string} keyspaceName The name of the keyspace to get ranges for.
      * @param {Host} host The host.
-     * @returns {Set<token.TokenRange> | null} Ranges for the keyspace on this host or null if
-     * keyspace isn't found or hasn't been loaded.
+     * @returns {Set<TokenRange> | null} Ranges for the keyspace on this host or null if keyspace
+     * isn't found or hasn't been loaded.
      */
     getTokenRangesForHost(
         keyspaceName: string,
         host: Host,
-    ): Set<token.TokenRange> | null {
+    ): Set<TokenRange> | null {
         throw new Error("TODO: Not implemented");
     }
 
@@ -118,19 +118,19 @@ class Metadata {
      * Constructs a Token from the input buffer(s) or string input. If a string is passed in
      * it is assumed this matches the token representation reported by cassandra.
      * @param {Buffer[] | Buffer | string} components The token components.
-     * @returns {token.Token} Constructed token from the input buffer.
+     * @returns {Token} Constructed token from the input buffer.
      */
-    newToken(components: Buffer[] | Buffer | string): token.Token {
+    newToken(components: Buffer[] | Buffer | string): Token {
         throw new Error("TODO: Not implemented");
     }
 
     /**
      * Constructs a TokenRange from the given start and end tokens.
-     * @param {token.Token} start The start token.
-     * @param {token.Token} end The end token.
-     * @returns {token.TokenRange} Build range spanning from start (exclusive) to end (inclusive).
+     * @param {Token} start The start token.
+     * @param {Token} end The end token.
+     * @returns {TokenRange} Build range spanning from start (exclusive) to end (inclusive).
      */
-    newTokenRange(start: token.Token, end: token.Token): token.TokenRange {
+    newTokenRange(start: Token, end: Token): TokenRange {
         throw new Error("TODO: Not implemented");
     }
 
